95bb1b8d10029f0e5c7195eb6b0d937da1df1c82
Rafal <rafal.pienkowski.mail@gmail.com>
2020-09-05 12:33:47 +0200

    Initial commit

---
+++ src/test/java/com/superdevs/SimpleDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
